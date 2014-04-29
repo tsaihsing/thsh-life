cd8dc6c9342d9857b2f1658428bdfba1005a8338
Shouko <h999342@gmail.com>
2014-04-29 15:56:43 +0800

    fix bounce problem by disabling overscroll in config.xml, and add a fixed bar at top when ios 7 detected

--- platforms/ios/Thsh Life/Classes/MainViewController.m
+++ platforms/ios/Thsh Life/Classes/MainViewController.m
@@ -65,11 +65,24 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
-    // you can do so here.
+// View defaults to full size. If you want to customize the view's size, or its subviews (e.g. webView),
+// you can do so here.
 
-    [super viewWillAppear:animated];
+// handle iOS 7 transparent status bar
+// according to http://stackoverflow.com/questions/19209781/ios-7-status-bar-with-phonegap
+//Lower screen 20px on ios 7
+/*
+if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7) {
+CGRect viewBounds = [self.webView bounds];
+viewBounds.origin.y = 20;
+viewBounds.size.height = viewBounds.size.height - 20;
+self.webView.frame = viewBounds;
 }
+*/
+
+[super viewWillAppear:animated];
+}
+
 
 - (void)viewDidLoad
 {
@@ -170,5 +183,7 @@
 {
     return [super execute:command];
 }
+
+
 
 @end
