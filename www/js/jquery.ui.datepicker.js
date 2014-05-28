de116a3a54bd86b348f8dc454c3b7b58700fd8ea
Shouko <h999342@gmail.com>
2014-05-28 08:41:33 +0800

    show day name while using datepicker

--- www/js/jquery.ui.datepicker.js
+++ www/js/jquery.ui.datepicker.js
@@ -50,7 +50,7 @@ function Datepicker() {
 		dayNamesShort: ["日", "一", "二", "三", "四", "五", "六"], // For formatting
 		dayNamesMin: ["日","一","二","三","四","五","六"], // Column headings for days starting at Sunday
 		weekHeader: "Wk", // Column header for week of the year
-		dateFormat: "yy/mm/dd", // See format options on parseDate
+		dateFormat: "yy/mm/dd (D)", // See format options on parseDate
 		firstDay: 0, // The first day of the week, Sun = 0, Mon = 1, ...
 		isRTL: false, // True if right-to-left language, false if left-to-right
 		showMonthAfterYear: false, // True if the year select precedes month, false for month then year
